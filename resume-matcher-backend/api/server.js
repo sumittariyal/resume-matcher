067b421bfc6e86f89c12a56a684744eab97ce4e9
Sumit Tariyal <sumittariyal11@gmail.com>
2026-03-14 16:09:17 +0530

    update

--- resume-matcher-backend/api/server.js
+++ resume-matcher-backend/api/server.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.json({ message: "Backend running" });
+  res.json({ message: "Backend running 🚀" });
 });
 
 export default app;
